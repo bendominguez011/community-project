27862408c0ac65b18d0ee76d8db888a0058128eb
Benjamin Dominguez <bendominguez011@gmail.com>
2015-12-28 22:50:28 -0500

    made changes to db create file

--- db_dev_create.py
+++ db_dev_create.py
@@ -8,7 +8,10 @@ post = Posts("How to Hook Grip with Mark Robb",
     "This is a video on how to hook grip with mark robb, https://www.youtube.com/watch?v=drGcGdSMeOg",
     author=u,
     community=c)
-db.session.add_all([u, c, post])
+comment = Comments("Testing the new commenting feature!",
+            author=u,
+            post=post)
+db.session.add_all([u, c, post, comment])
 c = Community(name="Programming", password=None, founder=u, FAQ=None, description=None)
 post = Posts("Rubber Ducky Code -- Intro to Flask",
             "An intro to the flask microframework, made for those just finished with Learn Python the \
@@ -19,6 +22,12 @@ post2 = Posts("Project Euler Solutions made in python",
             "Project euler solutions made in python can be found here https://github.com/bendominguez011/Project-Euler-Solutions",
             author=u,
             community=c)
-db.session.add_all([c, post, post2])
+comment1 = Comments("Testing the new commenting feature!",
+            author=u,
+            post=post)
+comment2 = Comments("Testing the new commenting feature!",
+            author=u,
+            post=post2)
+db.session.add_all([c, post, post2, comment1, comment2])
 
 db.session.commit()
